967e3ed224c06221eca1ade703dd8e730f41d18e
Thomas McPhee <mcp.thom@gmail.com>
2019-02-28 13:25:50 +0000

    created test file and a setUp function

---
+++ src/test/java/MunroTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+public class MunroTest {
+
+    Munro munro;
+
+    @Before
+    public void setUp(){
+        munro = new Munro("Ben Chonzie", 931, "MUN", "NN773308");
+    }
+
+    
+
+
+}
