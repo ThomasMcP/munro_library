cd19bfe64621b0b95dbd3695a03df2a357705382
Thomas McPhee <mcp.thom@gmail.com>
2019-02-28 13:44:49 +0000

    created constructor with getters and setters for List of Munros. Created initial test file.

---
+++ src/main/java/MunroList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class MunroList {
+
+    private ArrayList<Munro> munroList;
+
+    public MunroList(ArrayList<Munro> munroList) {
+        this.munroList = munroList;
+    }
+
+    public ArrayList<Munro> getMunroList() {
+        return munroList;
+    }
+
+    public void setMunroList(ArrayList<Munro> munroList) {
+        this.munroList = munroList;
+    }
+}
